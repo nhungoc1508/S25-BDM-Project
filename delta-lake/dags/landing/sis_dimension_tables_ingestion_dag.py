0f3f5e7b88923cfde0a89e00624093a09b1e5de7
Ngoc Hoang <nnh245@nyu.edu>
2025-05-25 21:42:55 +0000

    Reorganized landing jobs and DAGs

---
+++ delta-lake/dags/landing/sis_dimension_tables_ingestion_dag.py
@@ -0,0 +1,52 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2025, 4, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+spark_configs = {
+    "spark.master": "spark://spark-master:7077",
+    "spark.driver.extraClassPath": "/opt/bitnami/spark/jars/postgresql-42.6.0.jar:/opt/bitnami/spark/jars/delta-spark_2.12-3.0.0.jar:/opt/bitnami/spark/jars/delta-storage-3.0.0.jar",
+    "spark.executor.extraClassPath": "/opt/bitnami/spark/jars/postgresql-42.6.0.jar:/opt/bitnami/spark/jars/delta-spark_2.12-3.0.0.jar:/opt/bitnami/spark/jars/delta-storage-3.0.0.jar",
+    "spark.jars": "/opt/airflow/spark/jars/postgresql-42.6.0.jar,/opt/bitnami/spark/jars/delta-spark_2.12-3.0.0.jar,/opt/bitnami/spark/jars/delta-storage-3.0.0.jar",
+    "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
+    "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+
+    "spark.submit.deployMode": "client",
+    "spark.driver.host": "airflow-worker",
+    "spark.dynamicAllocation.enabled": "false",
+    "spark.executor.instances": "1",
+    "spark.executor.cores": "1",
+    "spark.executor.memory": "512m",
+    "spark.memory.fraction": "0.5",
+    "spark.memory.storageFraction": "0.3",
+    "spark.executor.memoryOverhead": "512m",
+
+    "spark.speculation": "true",
+    "spark.sql.shuffle.partitions": "2",
+    "spark.default.parallelism": "2",
+
+    "spark.driver.memory": "512m",
+}
+
+with DAG(dag_id="sis_dimension_tables_ingestion",
+         tags=["ingestion", "student information system"],
+         default_args=default_args,
+        #  schedule="@daily",
+         schedule=None,
+         catchup=False) as dag:
+    
+    task_ingest_dim_tables = SparkSubmitOperator(
+        task_id="submit_task_ingest_dim_tables",
+        application="/opt/airflow/spark/landing/ingest_sis_dimension_tables.py",
+        conn_id="spark-default",
+        application_args=[],
+        conf=spark_configs
+    )
+
+    task_ingest_dim_tables
---
+++ delta-lake/spark/landing/ingest_sis_dimension_tables.py
@@ -0,0 +1,70 @@
+from pyspark.sql import SparkSession
+from delta import *
+from pyspark.sql.functions import current_timestamp, current_date, lit, col
+from datetime import datetime
+import uuid
+import os
+import json
+
+from metadata_manager import add_metadata_entry
+
+BASE_PATH = 'file:///data/landing'
+DIMENSION_TABLES = ['academic_years', 'courses', 'degree_names', 'departments', 'faculty', 'students']
+
+def init_spark():
+    builder = SparkSession.builder \
+        .appName("SISDimensionTablesIngestion") \
+        .config("spark.driver.bindAddress", "0.0.0.0") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    return spark
+
+def ingest_data(spark, source_name, table_name):
+    print(f'[INGESTION TASK] Ingesting data for source: {source_name}')
+    batch_id = str(uuid.uuid4())
+    ingestion_timestamp = datetime.now()
+    ingestion_date = ingestion_timestamp.strftime("%Y-%m-%d")
+
+    landing_path = f'{BASE_PATH}/{source_name}/date={ingestion_date}/batch={batch_id}'
+    os.makedirs(landing_path, exist_ok=True)
+
+    df = spark.read.format("jdbc") \
+        .option("url", "jdbc:postgresql://postgres_sis:5432/university")\
+        .option("dbtable", table_name)\
+        .option("user", "admin") \
+        .option("password", "password") \
+        .option("driver", "org.postgresql.Driver") \
+        .load()
+    
+    df = df.withColumn("_source_name", lit(source_name)) \
+           .withColumn("_batch_id", lit(batch_id)) \
+           .withColumn("_ingestion_timestamp", lit(ingestion_timestamp.isoformat())) \
+           .withColumn("_ingestion_date", lit(ingestion_date))
+    
+    df.write \
+        .format('delta') \
+        .mode('overwrite') \
+        .save(landing_path)
+    print(f'[INGESTION TASK] Successfully ingested data to {landing_path}')
+
+    record_count = df.count()
+    metadata = {
+        "source_name": source_name,
+        "batch_id": batch_id,
+        "ingestion_timestamp": ingestion_timestamp.isoformat(),
+        "ingestion_date": ingestion_date,
+        "record_count": record_count,
+        "landing_path": landing_path,
+    }
+    add_metadata_entry(metadata)
+
+    print(f'[INGESTION TASK] Ingestion completed for source {source_name} with batch ID: {metadata["batch_id"]}')
+
+if __name__ == '__main__':
+    spark = init_spark()
+    for table in DIMENSION_TABLES:
+        ingest_data(spark, f'sis_{table}', table)
+    
+    spark.sparkContext.stop()
